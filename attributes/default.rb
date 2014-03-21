3391baaeb0aea2b18879163dea95d01c47c0a21e
mjsuhonos <mj@suhonos.ca>
2014-03-20 22:29:23 -0400

    Tweaks to find wonky module ; WIP

--- attributes/default.rb
+++ attributes/default.rb
@@ -65,52 +65,64 @@ default['islandora']['repos'] = [
   'islandora_solr_facet_pages',
 #  'islandora_solr_views', # Solr Views is not part of foundation yet
 
+  # core/dependency modules
   'islandora_xml_forms',
-#  'islandora_xmlsitemap',
-  'islandora_xacml_editor',
   'islandora_marcxml',
   'islandora_oai',
   'islandora_ocr',
   'islandora_batch',
   'islandora_bookmark',
   'islandora_importer',
-  'islandora_jwplayer',
-  'islandora_bagit',
+  'islandora_scholar',
+  'islandora_paged_content',
+
+#  'islandora_jwplayer',
+#  'islandora_sync',
+#  'islandora_simple_workflow',
+#  'islandora_xacml_editor',
+#  'islandora_internet_archive_bookreader',
+
+  # preservation modules
   'islandora_premis',
   'islandora_fits',
   'islandora_checksum',
   'islandora_checksum_checker',
-  'islandora_sync',
-  'islandora_scholar',
-  'islandora_paged_content',
-  'islandora_simple_workflow',
-  'islandora_internet_archive_bookreader',
-#  'islandora_openseadragon',
 
   # solution packs
   'islandora_solution_pack_collection',
+  'islandora_solution_pack_pdf',
   'islandora_solution_pack_audio',
-  'islandora_solution_pack_book',
+  'islandora_solution_pack_book', # depends on islandora_paged_content
   'islandora_solution_pack_compound',
   'islandora_solution_pack_image',
   'islandora_solution_pack_large_image',
   'islandora_solution_pack_newspaper',
-  'islandora_solution_pack_pdf',
   'islandora_solution_pack_video',
   'islandora_solution_pack_web_archive',
-  
+
+=begin
+  # these have to go at the end
   'islandora_book_batch',  
   'islandora_image_annotation',
+=end
+=begin
+  # these modules have funky dependencies
+  'islandora_openseadragon',
+  'islandora_xmlsitemap',
+  'islandora_bagit',
+=end
+
 ]
 
 # Islandora modules that need to be explicitly enabled
+# NB: ORDER MATTERS HERE FOR DEPENDENCIES
 default['islandora']['modules'] = [
   'xml_forms',
   'xml_form_builder',
   'xml_schema_api',
   'xml_form_elements',
   'xml_form_api',
-  'zip_importer',
+  'zip_importer', # depends on islandora_solution_pack_collection
   'islandora_basic_image',
   'islandora_solr_config',
   'jquery_update',
