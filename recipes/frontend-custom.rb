2651c1e8903fd35e7bc1cf21b866ab7cb8e24d38
paulwc <pwchurch@gmail.com>
2014-04-28 11:25:01 -0400

    more commenting, small path fix

--- recipes/frontend-custom.rb
+++ recipes/frontend-custom.rb
@@ -158,7 +158,7 @@ execute "tesseract build from source" do
   ignore_failure false
 end
 
-# download tesseract language data (english) and place it in the tesseract directory
+# download tesseract language data (english)
 ark 'tesseract-ocr-english-language-data' do
   url "https://tesseract-ocr.googlecode.com/files/tesseract-ocr-#{node['tesseract_engdata']['version']}.eng.tar.gz"
   checksum node['tesseract_engdata']['sha256']
@@ -167,6 +167,7 @@ ark 'tesseract-ocr-english-language-data' do
   action :put
 end
 
+# place it in the tesseract directory
 execute "move-english-language-files" do
   environment ({'DEBIAN_FRONTEND' => 'noninteractive'})
   command "cd #{node['tesseract_engdata']['installpath']} && sudo mv tesseract-ocr-english-language-data/* . && sudo rm -rf tesseract-ocr-english-language-data"
